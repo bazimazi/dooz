027139d40a0403bb74ed28215482e7801be96d4d
Hanieh1995 <hony1374@gmail.com>
2023-02-26 23:22:15 +0330

    uptimizing condition

--- React/src/app/pages/game/components/Header/Header.tsx
+++ React/src/app/pages/game/components/Header/Header.tsx
@@ -14,19 +14,15 @@ export function Header({ currentPlayer }: Props) {
   if (currentPlayer === P1) {
     playerOneClasses = [classes.player1, classes.playerOneTurn];
     playerTwoClasses = [classes.player2];
-    showVectorPink = classes.showVectorPink;
-    showVectorYellow = '';
   } else {
     playerTwoClasses = [classes.player2, classes.playerTwoTurn];
     playerOneClasses = [classes.player1];
-    showVectorYellow = classes.showVectorYellow;
-    showVectorPink = '';
   }
 
   return (
     <>
       <div className={playerOneClasses.join(' ')}>
-        <img src="static/images/Vector_pink.svg" alt="Player 1 Turn" className={showVectorPink} />
+        <img src="static/images/Vector_pink.svg" alt="Player 1 Turn"  className={currentPlayer === P1 ? classes.showVectorPink : ''} />
         <div className={classes.playerContainer}>
           <img src="static/images/player.svg" alt="Player 1" />
           <div className={classes.playerTitle}>Player 1</div>
@@ -37,7 +33,7 @@ export function Header({ currentPlayer }: Props) {
       <div className={classes.vs}>VS</div>
 
       <div className={playerTwoClasses.join(' ')}>
-        <img src="static/images/Vector_yellow.svg" alt="Player 2 Turn" className={showVectorYellow} />
+        <img src="static/images/Vector_yellow.svg" alt="Player 2 Turn" className={currentPlayer === P2 ? classes.showVectorYellow : ''} />
         <div className={classes.playerContainer}>
           <img src="static/images/player.svg" alt="Player 2" />
           <div className={classes.playerTitle}>Player 2</div>
