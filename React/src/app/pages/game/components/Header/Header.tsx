e58e8d09632ee195971f7b62866a146d43746147
Hanieh1995 <hony1374@gmail.com>
2023-02-28 12:29:18 +0330

    Optimizing conditions

--- React/src/app/pages/game/components/Header/Header.tsx
+++ React/src/app/pages/game/components/Header/Header.tsx
@@ -6,23 +6,11 @@ interface Props {
 }
 
 export function Header({ currentPlayer }: Props) {
-  let playerOneClasses: String[] = [];
-  let playerTwoClasses: String[] = [];
-  let showVectorPink;
-  let showVectorYellow;
-
-  if (currentPlayer === P1) {
-    playerOneClasses = [classes.player1, classes.playerOneTurn];
-    playerTwoClasses = [classes.player2];
-  } else {
-    playerTwoClasses = [classes.player2, classes.playerTwoTurn];
-    playerOneClasses = [classes.player1];
-  }
 
   return (
     <>
-      <div className={playerOneClasses.join(' ')}>
-        <img src="static/images/Vector_pink.svg" alt="Player 1 Turn"  className={currentPlayer === P1 ? classes.showVectorPink : ''} />
+      <div className={`${classes.player} ${currentPlayer === P1 ? classes.playerOneTurn : ''}`}>
+        <img src="static/images/Vector_pink.svg" alt="Player 1 Turn" className={classes.vector} />
         <div className={classes.playerContainer}>
           <img src="static/images/player.svg" alt="Player 1" />
           <div className={classes.playerTitle}>Player 1</div>
@@ -32,8 +20,8 @@ export function Header({ currentPlayer }: Props) {
 
       <div className={classes.vs}>VS</div>
 
-      <div className={playerTwoClasses.join(' ')}>
-        <img src="static/images/Vector_yellow.svg" alt="Player 2 Turn" className={currentPlayer === P2 ? classes.showVectorYellow : ''} />
+      <div className={`${classes.player} ${currentPlayer === P2 ? classes.playerTwoTurn : ''}`}>
+        <img src="static/images/Vector_yellow.svg" alt="Player 2 Turn" className={classes.vector} />
         <div className={classes.playerContainer}>
           <img src="static/images/player.svg" alt="Player 2" />
           <div className={classes.playerTitle}>Player 2</div>
