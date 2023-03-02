7864421a239041b05729a9d95acac1e8b6cb9005
Saleh Yusefnejad <msynk@outlook.com>
2023-03-02 23:01:39 +0330

    Add current player indicator to the Vanilla implementation #19

--- React/src/app/pages/game/components/Header/Header.tsx
+++ React/src/app/pages/game/components/Header/Header.tsx
@@ -9,8 +9,8 @@ export function Header({ currentPlayer }: Props) {
 
   return (
     <>
-      <div className={`${classes.player} ${currentPlayer === P1 ? classes.playerOneTurn : ''}`}>
-        <img src="static/images/Vector_pink.svg" alt="Player 1 Turn" className={classes.vector} />
+      <div className={`${classes.player1} ${currentPlayer === P1 ? classes.current : ''}`}>
+        <img src="static/images/current_1.svg" alt="Player 1 Turn" className={classes.currentImg} />
         <div className={classes.playerContainer}>
           <img src="static/images/player.svg" alt="Player 1" />
           <div className={classes.playerTitle}>Player 1</div>
@@ -20,8 +20,8 @@ export function Header({ currentPlayer }: Props) {
 
       <div className={classes.vs}>VS</div>
 
-      <div className={`${classes.player} ${currentPlayer === P2 ? classes.playerTwoTurn : ''}`}>
-        <img src="static/images/Vector_yellow.svg" alt="Player 2 Turn" className={classes.vector} />
+      <div className={`${classes.player2} ${currentPlayer === P2 ? classes.current : ''}`}>
+        <img src="static/images/current_2.svg" alt="Player 2 Turn" className={classes.currentImg} />
         <div className={classes.playerContainer}>
           <img src="static/images/player.svg" alt="Player 2" />
           <div className={classes.playerTitle}>Player 2</div>
