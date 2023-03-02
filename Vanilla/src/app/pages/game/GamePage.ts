9024c6b78195a7bc06d18ca3ce70adf0658727de
Saleh Yusefnejad <msynk@outlook.com>
2023-03-02 23:03:33 +0330

    feat(vanilla): add current player indicator to the Vanilla implementation #19 (#21)

--- Vanilla/src/app/pages/game/GamePage.ts
+++ Vanilla/src/app/pages/game/GamePage.ts
@@ -5,17 +5,21 @@ import { WinResult } from "$/WinResult";
 import { P1, P2 } from "$/players";
 import { checkBoard } from "$/check-board";
 import { Board } from "./components/board/Board";
+import { Header } from "./components/header/Header";
 
 
 export class GamePage extends ShadowedElement {
     private boardData: any[][] = [[]];
     private result: WinResult | null = null;
     private currentPlayer: string = Math.round(Math.random()) ? P1 : P2;
+    private header: Header;
     private board: Board;
 
     constructor() {
         super(html, css);
 
+        this.header = this.shadowRoot!.querySelector('.header')!;
+        this.header.current = this.currentPlayer;
         this.board = this.shadowRoot!.querySelector('.body')!;
         this.board?.addEventListener('cell-click', (e: any) => {
             const { i, j } = e.detail;
@@ -31,20 +35,21 @@ export class GamePage extends ShadowedElement {
         if (this.boardData[i][j] || this.result) return;
 
         this.boardData[i][j] = this.currentPlayer;
+        this.board.data = { i, j, player: this.currentPlayer.toLocaleLowerCase() };
+        if (this.checkWinner()) return;
+
         this.currentPlayer = this.currentPlayer === P1 ? P2 : P1;
-
-        this.board.data = { i, j, player: (this.boardData[i][j]).toLowerCase() };
-
-        this.checkWinner();
+        this.header.current = this.currentPlayer;
     };
 
     private checkWinner() {
         const winResult = checkBoard(this.boardData);
-        if (!winResult) return;
+        if (!winResult) return false;
 
         this.result = winResult;
         // winResult.line.forEach((cell) => {
         //     this.boardData[cell[0]][cell[1]] = "🤩";
         // });
+        return true;
     };
 }
--- Vanilla/src/app/pages/game/components/header/Header.ts
+++ Vanilla/src/app/pages/game/components/header/Header.ts
@@ -1,9 +1,31 @@
 import { ShadowedElement } from "$/ShadowedElement";
 import html from "./Header.html?raw";
 import css from "./Header.css?raw";
+import { P1 } from "$/players";
 
 export class Header extends ShadowedElement {
+    private _player1: HTMLElement;
+    private _player2: HTMLElement;
+
     constructor() {
         super(html, css);
+
+        this._player1 = this.shadowRoot!.querySelector('.player1')!;
+        this._player2 = this.shadowRoot!.querySelector('.player2')!;
     }
+
+    static get observedAttributes() { return ['current'] }
+
+    get current() { return this.getAttribute('current')!; }
+    set current(value: string) {
+        this.setAttribute('current', value);
+        if (value === P1) {
+            this._player1.classList.add('current');
+            this._player2.classList.remove('current');
+        } else {
+            this._player1.classList.remove('current');
+            this._player2.classList.add('current');
+        }
+    }
+
 }
