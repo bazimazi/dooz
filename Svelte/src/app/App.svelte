18c5a8e7fd2a2614fcb01c2c857bf46dd957e21d
Saleh Yusefnejad <msynk@outlook.com>
2023-03-07 18:13:07 +0330

    add initial implementation of Svelte

---
+++ Svelte/src/app/App.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<div class="main">Hello Svelte!</div>
+
+<style>
+  .main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    margin: auto;
+    background-color: #232599;
+  }
+</style>
