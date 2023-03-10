d64536af44eb318c01facda5734bf0cbf2883982
Saleh Yusefnejad <msynk@outlook.com>
2023-03-10 18:46:28 +0330

    feat(vue): add initial Vuejs implementation #34 (#35)

---
+++ Vue/src/app/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="main">
+    Hello Vue!
+  </div>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  margin: auto;
+  background-color: #232599;
+}
+</style>
