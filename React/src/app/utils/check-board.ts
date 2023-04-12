4d4c1f54298148040c8088569989d5dcf151634b
amirMasud <49915745+amirmasud111@users.noreply.github.com>
2023-04-12 17:17:39 +0330

    feat(react): replace the winlines array with a dynamic function that can work for any game size #44 (#46)

--- React/src/app/utils/check-board.ts
+++ React/src/app/utils/check-board.ts
@@ -1,19 +1,9 @@
-
-const WIN_LINES = [
-    [[0, 0], [0, 1], [0, 2]],
-    [[1, 0], [1, 1], [1, 2]],
-    [[2, 0], [2, 1], [2, 2]],
-
-    [[0, 0], [1, 0], [2, 0]],
-    [[0, 1], [1, 1], [2, 1]],
-    [[0, 2], [1, 2], [2, 2]],
-
-    [[0, 0], [1, 1], [2, 2]],
-    [[0, 2], [1, 1], [2, 0]],
-];
+import { createWinLines } from "./create-winlines";
+const boardSize = 3;
+const winLines = createWinLines(boardSize);
 
 export function checkBoard(board: string[][], currentPlayer: string): number[][] | void {
 
-    return WIN_LINES.find(line => line.every(cell => board[cell[0]][cell[1]] == currentPlayer))
+    return winLines.find(line => line.every(cell => board[cell[0]][cell[1]] == currentPlayer))
 
 }
