6ae2380312dc8fe1d85c859fc746620e7a22dd69
Amirmasud <amirmasudpoolaee@yahoo.com>
2023-04-25 22:42:01 +0430

    optimizing

--- React/src/app/pages/game/BotGamePage/BotGamePage.tsx
+++ React/src/app/pages/game/BotGamePage/BotGamePage.tsx
@@ -63,49 +63,6 @@ export function BotGamePage() {
     setCurrentPlayer(P1);
   };
 
-  // function isAvailableCell(board: string[][]) {
-  //   return board.some(row => row.some(cell => cell == null));
-  // }
-
-  // function minimax(board: any, depth: number, isMaximizing: boolean) {
-  //   let current = isMaximizing ? P2 : P1;
-  //   let winResult = checkBoard(board, current);
-  //   if (winResult) {
-  //     if (isMaximizing) return 1;
-  //     if (!isMaximizing) return -1;
-  //   } else {
-  //     if (!isAvailableCell(board)) return 0
-  //   }
-  //   if (isMaximizing) {
-  //     let bestScore = -Infinity;
-  //     for (let i = 0; i < 3; i++) {
-  //       for (let j = 0; j < 3; j++) {
-  //         if (board[i][j] == null) {
-  //           board[i][j] = P2;
-  //           let score = minimax(board, depth + 1, false);
-  //           board[i][j] = null;
-  //           if (score > bestScore) bestScore = score;
-  //         }
-  //       }
-  //     }
-  //     return bestScore;
-  //   } else {
-  //     let bestScore = Infinity;
-  //     for (let i = 0; i < 3; i++) {
-  //       for (let j = 0; j < 3; j++) {
-  //         if (board[i][j] == null) {
-  //           board[i][j] = P1;
-  //           let score = minimax(board, depth + 1, true);
-  //           board[i][j] = null;
-  //           if (score < bestScore) bestScore = score;
-  //         }
-  //       }
-  //     }
-  //     return bestScore;
-  //   }
-  // }
-
-
   const checkWinner = (currentPlayer: string) => {
     const winResult = checkBoard(boardData, currentPlayer);
     if (!winResult) return false;
--- React/src/app/utils/minimax.ts
+++ React/src/app/utils/minimax.ts
@@ -5,40 +5,44 @@ function isAvailableCell(board: string[][]) {
     return board.some(row => row.some(cell => cell == null));
 }
 
-export function minimax(board: any, depth: number, isMaximizing: boolean) {
+export function minimax(board: any[][], depth: number, isMaximizing: boolean) {
     let current = isMaximizing ? P2 : P1;
     let winResult = checkBoard(board, current);
-    if (winResult) {
-        if (isMaximizing) return 1;
-        if (!isMaximizing) return -1;
-    } else {
-        if (!isAvailableCell(board)) return 0
-    }
-    if (isMaximizing) {
-        let bestScore = -Infinity;
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                if (board[i][j] == null) {
-                    board[i][j] = P2;
-                    let score = minimax(board, depth + 1, false);
-                    board[i][j] = null;
-                    if (score > bestScore) bestScore = score;
-                }
-            }
-        }
-        return bestScore;
-    } else {
-        let bestScore = Infinity;
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                if (board[i][j] == null) {
-                    board[i][j] = P1;
-                    let score = minimax(board, depth + 1, true);
-                    board[i][j] = null;
-                    if (score < bestScore) bestScore = score;
-                }
-            }
-        }
-        return bestScore;
-    }
+
+    if (winResult) return isMaximizing ? 1 : -1;
+    if (!isAvailableCell(board)) return 0;
+
+    if (isMaximizing) return findMaxMove(board, depth);
+    return findMinMove(board, depth)
+
 }
+function findMaxMove(board: any[][], depth: number) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] == null) {
+                board[i][j] = P2;
+                let score = minimax(board, depth + 1, false);
+                board[i][j] = null;
+                bestScore = Math.max(bestScore, score);
+            }
+        }
+    }
+    return bestScore;
+}
+
+function findMinMove(board: any[][], depth: number) {
+    let bestScore = Infinity;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] == null) {
+                board[i][j] = P1;
+                let score = minimax(board, depth + 1, true);
+                board[i][j] = null;
+                bestScore = Math.min(bestScore, score);
+            }
+        }
+    }
+    return bestScore;
+}
+
