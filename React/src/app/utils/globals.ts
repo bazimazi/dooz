b03ad0f1b48a32448c8596407d6c47959db3f590
Amirmasud <amirmasudpoolaee@yahoo.com>
2023-10-23 08:40:33 +0330

    make globals readonly for safety

--- React/src/app/utils/globals.ts
+++ React/src/app/utils/globals.ts
@@ -4,6 +4,6 @@ export const globals = {
     padding: 12,
     boardWidth: 312,
     boardBorderWidth: 1,
-    playerVsPlayer:"/player-vs-player",
-    playerVsBot:"/player-vs-bot"
-}
+    playerVsPlayer: "/player-vs-player",
+    playerVsBot: "/player-vs-bot"
+} as const
