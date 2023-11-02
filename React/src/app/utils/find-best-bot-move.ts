538776acb2af52660bf5dadb55e0c04412cb8bc3
amirMasud <49915745+amirmasud111@users.noreply.github.com>
2023-11-02 08:13:02 +0330

    feat: improve the minimax algorithm #61 (#62)

--- React/src/app/utils/find-best-bot-move.ts
+++ React/src/app/utils/find-best-bot-move.ts
@@ -1,19 +1,16 @@
 import { P1, P2 } from "./players";
 import { checkBoard } from "./check-board";
 import { anyMovesLeft } from "./any-moves-left";
-import { getWinLines } from "./get-winLines";
-
-let winLinesArray: number[][][];
 
 export function findBestBotMove(board: any[][]) {
-    winLinesArray = getWinLines(board.length);
+    const memo = new Map();
     let bestScore = -Infinity;
     let bestBotMove;
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board.length; j++) {
             if (board[i][j] == null) {
                 board[i][j] = P2;
-                let score = minimax(board, 0, false);
+                let score = minimax(board, 0, false, -Infinity, Infinity, memo);
                 board[i][j] = null;
                 if (score > bestScore) {
                     bestScore = score;
@@ -25,47 +22,36 @@ export function findBestBotMove(board: any[][]) {
     return bestBotMove;
 }
 
-function minimax(board: any[][], depth: number, isMaximizing: boolean) {
-
-    let current = isMaximizing ? P2 : P1;
-    let winResult = winLinesArray && checkBoard(board, current);
-
-    if (winResult) return isMaximizing ? 1 : -1;
-    if (anyMovesLeft(board)) return 0;
-
-    if (isMaximizing) return findMaxMove(board, depth);
-    return findMinMove(board, depth)
-
-}
-
-function findMaxMove(board: any[][], depth: number) {
-
-    let bestScore = -Infinity;
+function minimax(board: any[][], depth: number, isMaximizing: boolean, alpha: number, beta: number, memo: Map<any, any>) {
+    let currentPlayer = isMaximizing ? P1 : P2;
+    const key = board.toString();
+    if (memo.has(key)) return memo.get(key);
+    if (checkBoard(board, currentPlayer)) return isMaximizing ? -100 + depth : 100 - depth;
+    if (anyMovesLeft(board) || depth >= board.length) return 0;
+    let bestScore = isMaximizing ? -Infinity : Infinity;
+    outerLoop:
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board.length; j++) {
-            if (board[i][j] == null) {
-                board[i][j] = P2;
-                let score = minimax(board, depth + 1, false);
+            if (board[i][j] === null) {
+                board[i][j] = isMaximizing ? P2 : P1;
+                let score = minimax(board, depth + 1, !isMaximizing, alpha, beta, memo);
                 board[i][j] = null;
-                bestScore = Math.max(bestScore, score);
+                bestScore = isMaximizing ? Math.max(bestScore, score) : Math.min(bestScore, score);
+                if (isMaximizing) {
+                    alpha = Math.max(alpha, score);
+                    if (beta <= alpha) {
+                        break outerLoop;
+                    }
+                } else {
+                    beta = Math.min(beta, score);
+                    if (beta <= alpha) {
+                        break outerLoop;
+                    }
+                }
             }
         }
     }
-    return bestScore;
-}
-
-function findMinMove(board: any[][], depth: number) {
-    let bestScore = Infinity;
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board.length; j++) {
-            if (board[i][j] == null) {
-                board[i][j] = P1;
-                let score = minimax(board, depth + 1, true);
-                board[i][j] = null;
-                bestScore = Math.min(bestScore, score);
-            }
-        }
-    }
+    memo.set(key, bestScore);
     return bestScore;
 }
 
